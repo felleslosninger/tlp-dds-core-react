58860fbfafe7b473065385ac2c2582a6ab1858e9
Bendik Kvernevik <bendiksem93@gmail.com>
2022-02-28 17:17:52 +0100

    feat(button): add component setup
    * Configure deps: Storybook, React, Sass, Typescript
    * Setup Webpack config to resolve CSS-modules in SCSS
    * Add Button skeleton with CSS module from dds-core-css and story skeleton

---
+++ .storybook/main.js
@@ -0,0 +1,33 @@
+module.exports = {
+  stories: [
+    "../src/**/stories/*.stories.mdx",
+    "../src/**/stories/*.stories.@(js|jsx|ts|tsx)",
+  ],
+  addons: [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/addon-interactions",
+  ],
+  framework: "@storybook/react",
+  core: {
+    builder: "webpack5",
+  },
+  // Add rules to webpack config
+  webpackFinal: async (config, { configType }) => {
+    // Resolve raw import of SVG files as source code
+    config.module.rules.push({
+      test: /\.svg/,
+      type: "asset/source",
+    })
+    // Resolve SCSS as CSS modules
+    config.module.rules.push({
+      test: /\.(scss)$/,
+      use: [
+        { loader: "style-loader" }, // Inject result into DOM as style block
+        { loader: "css-loader", options: { modules: true } }, // Convert CSS to Javascript to be bundled and rename to CSS modules
+        { loader: "sass-loader" }, // Convert SCSS to CSS
+      ],
+    })
+    return config
+  },
+}
---
+++ src/Button/Button.tsx
@@ -0,0 +1,15 @@
+import React from "react"
+import styles from "@digdir/ds-core-css/build/src/button.scss"
+
+export interface ButtonProps
+  extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+  children?: React.ReactNode
+}
+
+const Button = ({ children }: ButtonProps) => {
+  const classes = styles["dds-button"]
+
+  return <button className={classes}> {children} </button>
+}
+
+export default Button
